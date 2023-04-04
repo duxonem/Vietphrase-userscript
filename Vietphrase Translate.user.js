a3bfcc821fcb48a6c4defafa62c5923d62800bf0
duxonem <128269506+duxonem@users.noreply.github.com>
2023-04-04 11:47:26 +0700

    Update Vietphrase Translate.user.js

--- Vietphrase Translate.user.js
+++ Vietphrase Translate.user.js
@@ -8,7 +8,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // @run-at       document-idle
-// @require      https://pastebin.com/raw/15YcP05S
+// @require      https://raw.githubusercontent.com/duxonem/Vietphrase-userscript/main/PhienAm.txt.js
 // @require      https://raw.githubusercontent.com/duxonem/Vietphrase-userscript/main/Names.txt
 // @require      https://raw.githubusercontent.com/duxonem/Vietphrase-userscript/main/VietPhrase.txt.js
 // ==/UserScript==
@@ -53,12 +53,13 @@ if (!domNode) return;
         domNode.nodeValue = translate(domNode.nodeValue,Names);
         domNode.nodeValue = translate(domNode.nodeValue,VietPhrase,true);
         domNode.nodeValue = translate(domNode.nodeValue,PhienAm);
+        //domNode.setAttribute("word-wrap","break-word");  // khong work
+        //domNode.style.wordBreak="break-word";  // khong work
         return; }
 
       if (domNode.nodeType != 1) return;
 
-      if (domNode.tagName && ',OBJECT,FRAME,FRAMESET,IFRAME,SCRIPT,EMBD,STYLE,BR,HR,TEXTAREA,'.indexOf(',' + domNode.tagName.toUpperCase() + ',') > - 1)
-        return;
+      if (domNode.tagName && ',OBJECT,FRAME,FRAMESET,IFRAME,SCRIPT,EMBD,STYLE,BR,HR,TEXTAREA,'.indexOf(',' + domNode.tagName.toUpperCase() + ',') > - 1) return;
 
       //if (domNode.title)      domNode.title = translate(domNode.title,PhienAm);
 
@@ -67,21 +68,15 @@ if (!domNode) return;
       if (domNode.tagName && domNode.type && domNode.tagName.toUpperCase() == 'INPUT' && ',button,submit,reset,'.indexOf(domNode.type.toLowerCase()) > - 1)
         domNode.value = translate(domNode.value,PhienAm);
 
-      for (var i = 0, j = domNode.childNodes.length; i < j; i++)
-        translateNode(domNode.childNodes[i]);
+      for (var i = 0, j = domNode.childNodes.length; i < j; i++) translateNode(domNode.childNodes[i]);
 }
 
 
 (function() {
     'use strict';
 
-    // Your code here...
-    const transButton=document.createElement("button");
-    transButton.innerText="Translate";
-    transButton.style='background-color:#ff4545; position: fixed; top:1%; right:1%; z-index: 99999;';
-
     let mouseClick2 = function () {
-        document.title= translate(document.title,PhienAm)
+        document.title= translate(document.title,PhienAm);
         translateNode(document.body);
         console.log(translateCount);}
 
@@ -92,8 +87,22 @@ if (!domNode) return;
         document.body.innerHTML= translate(document.body.innerHTML,PhienAm); //PhienAm dat trong file require o header
         console.log(translateCount);}
 
+    const transButton=document.createElement("div");
+    transButton.style="position: fixed;top: 1%; right:1%; width:70px; height:25px; border: thin;z-index:99999";
+
+    const button1=document.createElement("button");
+    button1.innerText="Tran";
+    button1.style= "width:50%; height:90%; top: 0; left: 0; position:absolute; border: none; text-align:right; padding:1px;";
+    button1.onclick=mouseClick1;
+    transButton.appendChild(button1);
+
+    const button2=document.createElement("button");
+    button2.innerText="slate";
+    button2.style= "width:50%; height:90%; top: 0; right: 0; position:absolute; border: none; text-align:left; padding:1px;";
+    button2.onclick=mouseClick2;
+    transButton.appendChild(button2);
 
     document.body.appendChild(transButton);
-    transButton.onclick =mouseClick1;
+
 
 })();
