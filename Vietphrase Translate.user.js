c5bf65c83b1df653bb84209d08bec39f1a6c66ee
duxonem <128269506+duxonem@users.noreply.github.com>
2023-05-06 19:06:45 +0700

    Update Vietphrase Translate.user.js

--- Vietphrase Translate.user.js
+++ Vietphrase Translate.user.js
@@ -6,12 +6,25 @@
 // @author       You
 // @match        http*://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
 // @run-at       document-idle
 // @require      https://raw.githubusercontent.com/duxonem/Vietphrase-userscript/main/PhienAm.txt.js
 // @require      https://raw.githubusercontent.com/duxonem/Vietphrase-userscript/main/Names.txt
 // @require      https://raw.githubusercontent.com/duxonem/Vietphrase-userscript/main/VietPhrase.txt.js
 // ==/UserScript==
+
+// Option
+const Ngoac=false;         //dau ngoac bao quanh VietPhrase
+const VPmotnghia=true;    // neu VP co nhieu nghia, chi lay nghia dau tien
+const Daucachnghia=';';    //dau cach cac nghia trong cung Entry VP
+const Xoadichlieutru=true; //Xoa dich, lieu, tru
+let Options = {
+    Ngoac:false,
+    VPmotnghia:true,
+    Daucachnghia:';',
+    Xoadichlieutru:true };
 
 function htmlEntities(str) { return String(str).replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'","&#39;");}String.prototype.count=function(search) {return this.split(search).length-1;}
 function isLetter(str) { return str.length == 1 && str.match(/[0-9a-z]/i); }
@@ -19,18 +32,18 @@ function isChineseLetter(str) {return str.length==1 && str.match(/[\u4E00-\u9FA5
 
 let translateCount=0;
 
-function translateByOrder(text,bracket=true) {  //no use now, leave it here to check function translate if needed
+function translateByOrder(text,safe=true) {
             let result = [], tmpArr={};
 
             let Name;
             let Han=Object.keys(Names);
             Han.sort((a,b) => (b.length -a.length));
-            let maxLength=Han[0].length;
+            let maxLength=Han[0].length+1;
 
             for (let i=0; i<text.length; i++) {
-                if (text.charAt(i) == '\u0528') continue;  //break for
+                if (text.charAt(i) == '\u0528') continue;
                 for (let j=maxLength; j>1;j--) {
-                    let HanViet=Names[text.substring(i,(i+j))];
+                    let HanViet=Names[text.substring(i,(i+j))];  ////////////////
                     if (HanViet == undefined) continue;
                         tmpArr.pos=i;
                         tmpArr.orgText=text.substring(i,i+j);
@@ -41,35 +54,41 @@ function translateByOrder(text,bracket=true) {  //no use now, leave it here to c
                         text=text.replace(tmpArr.orgText,'\u0528'.repeat(tmpArr.orgText.length))
                         i+=tmpArr.orgText.length-1;
                         tmpArr={};
-                } //end for
+                }
+
             }//end for
 
             Han=Object.keys(VietPhrase);
             Han.sort((a,b) => (b.length -a.length));
-            maxLength=Han[0].length;
+            maxLength=Han[0].length+1;
 
             for (let i=0; i<text.length; i++) {
                 if (text.charAt(i) == '\u0528') continue;
                 for (let j=maxLength; j>1; j-- ) {
-                    let HanViet=VietPhrase[text.substring(i,(i+j))];
+                    let HanViet=VietPhrase[text.substring(i,(i+j))]; ////////////////
                     if (HanViet == undefined) continue;
                         tmpArr.pos=i;
                         tmpArr.orgText=text.substring(i,i+j);
-                        tmpArr.transText=HanViet;
+                        if (Options.VPmotnghia) tmpArr.transText=HanViet.split(Daucachnghia)[0]; // Mot nghia
+                        else tmpArr.transText=HanViet;  // nhieu nghia
+
+                        if (Options.Ngoac) tmpArr.transText='['+tmpArr.transText + ']';  //dau ngoac VP
                         tmpArr.dict='VP';
                         result.push(tmpArr);
 
                         text=text.replace(tmpArr.orgText,'\u0528'.repeat(tmpArr.orgText.length))
                         i+=tmpArr.orgText.length-1;
-                        tmpArr={}; // must create new object, the existing one is just pushed in to result[] by reference
-                } //end for
+                        tmpArr={}; // must create new object, the existing one is just pushed in to result by reference
+                }
             }//end for
 
 
+            let dichlieuArr =['的','了','著'];
+            if (Options.Xoadichlieutru) dichlieuArr.forEach(word => text.replaceAll(word,''));
             //convert PhienAm
             for (let i=0; i<text.length; i++) {
                 let char=text.charAt(i);
-                if (char=='\u0528') continue; 
+                if (char=='\u0528') continue; //bo mat khoang trang giua 2 chu, pahi thay lai. Chi xay ra khi chay tren cac ky tu kophai tieng trung quoc do tieng tq viet lien
 
                 while (text.indexOf(char) >=0 ) {
                     tmpArr.pos=text.indexOf(char);
@@ -82,24 +101,23 @@ function translateByOrder(text,bracket=true) {  //no use now, leave it here to c
                     result.push(tmpArr);
                     text=text.replace(char,'\u0528');
                     tmpArr={}; // must create new object
-                } //end while
+                }
             } //end for
 
             //result=[...new Set(result)];
             result.sort((a,b) => (a.pos -b.pos));
 
-            let returnText='';
-            for (let i=0;i<result.length; i++) {
-                if (bracket && result[i].dict=='VP') returnText+='['+ result[i].transText + '] ';
-                else if (isChineseLetter(result[i].orgText)) returnText+=result[i].transText + ' ';
-                else returnText+=result[i].transText; }
+            let returnText=result[0].transText;
+            for (let i=1;i<result.length; i++)
+               if (result[i].dict == 'FA' && result[i-1].dict == 'FA') returnText+=result[i].transText;
+               else returnText+=' ' + result[i].transText;
 
             translateCount++;
             return returnText;
-        }
+        } //end function
 
 
-function translate(text, dict,brackets=false,safe=false) {
+function translate(text, dict,safe=true) {
     let Viet='';
     text=text.replaceAll('&nbsp;',' ');
 
@@ -107,12 +125,17 @@ function translate(text, dict,brackets=false,safe=false) {
         if (safe) Viet =htmlEntities(dict[Han]);
         else Viet = dict[Han];
 
-        if (brackets) text=text.replaceAll(Han,'['+Viet+'] ');
+        if (Options.VPmotnghia) Viet=Viet.split(Daucachnghia)[0];
+
+        if (Options.Xoadichlieutru &&dict=='PhienAm') {
+            let dichlieuArr =['的','了','著'];
+            dichlieuArr.foreach(word => text.replaceAll(word,'')); }
+
+        if (Options.Ngoac && dict==VietPhrase) text=text.replaceAll(Han,'['+Viet+'] ');
         else text=text.replaceAll(Han,Viet+' ');
     } //End for
 
     let pointAtEnd=false;
-    
     if (text.charAt(text.length-1)=='。') pointAtEnd=true;
 
     let lines=text.split('。');
@@ -134,18 +157,22 @@ function translateNode(domNode) {
 
     function Imtired (domNode) {
         if (!domNode) return;
-        if (domNode.nodeType == 3) { stackToStockThings.push(domNode); return; }
+        if (domNode.nodeType == 3) {
+            stackToStockThings.push(domNode);
+            return; }
 
-        if (domNode.nodeType != 1) return;
-        if (domNode.tagName && ',OBJECT,FRAME,FRAMESET,IFRAME,SCRIPT,EMBD,STYLE,BR,HR,TEXTAREA,'.indexOf(',' + domNode.tagName.toUpperCase() + ',') > - 1) return;
-        if (domNode.tagName && domNode.type && domNode.tagName.toUpperCase() == 'INPUT' && ',button,submit,reset,'.indexOf(domNode.type.toLowerCase()) > - 1)  stackToStockThings.push(domNode);
+      if (domNode.nodeType != 1) return;
+      if (domNode.tagName && ',OBJECT,FRAME,FRAMESET,IFRAME,SCRIPT,EMBD,STYLE,BR,HR,TEXTAREA,'.indexOf(',' + domNode.tagName.toUpperCase() + ',') > - 1) return;
+      if (domNode.tagName && domNode.type && domNode.tagName.toUpperCase() == 'INPUT' && ',button,submit,reset,'.indexOf(domNode.type.toLowerCase()) > - 1)
+        domNode.value = translate(domNode.value,PhienAm);
 
-        for (var i = 0, j = domNode.childNodes.length; i < j; i++) Imtired(domNode.childNodes[i]);
+      for (var i = 0, j = domNode.childNodes.length; i < j; i++) Imtired(domNode.childNodes[i]);
     } //End function Imtired
 
     Imtired(domNode);
+    console.log(stackToStockThings.length);
     let text='', tmpArr=[];
-    for (let i=0; i<stackToStockThings.length; i++) text+=stackToStockThings[i].nodeValue + '\uf0f3'.repeat(1); //repeat 3 takes longer than repeat 2 ~~10%
+    for (let i=0; i<stackToStockThings.length; i++) text+=stackToStockThings[i].nodeValue + '\uf0f3'.repeat(1);  //repeat 3 takes longer than repeat 2 ~~10%
     text=translateByOrder(text);
     //text=translate(text,Names,false);
     //text=translate(text,VietPhrase,true);
@@ -169,27 +196,84 @@ function translateNode(domNode) {
         console.time('Translate 1');
         document.title= translate(document.title,PhienAm);
         document.body.innerHTML= translate(document.body.innerHTML,Names); //Names dat trong file require o header
-        document.body.innerHTML= translate(document.body.innerHTML,VietPhrase,true,true); //VietPhrase dat trong file require o header, ghi ra co []
+        document.body.innerHTML= translate(document.body.innerHTML,VietPhrase); //VietPhrase dat trong file require o header, ghi ra co []
         document.body.innerHTML= translate(document.body.innerHTML,PhienAm); //PhienAm dat trong file require o header
         console.timeEnd('Translate 1');
         console.log(translateCount);}
 
+    let menuClick= function () {
+        if (document.getElementById('menuBtn').innerText =='↓') {
+            document.getElementById('menuBtn').innerText= '↔';
+            menuOption.style.display="block";
+        } else {
+            document.getElementById('menuBtn').innerText ='↓';
+            menuOption.style.display="none";
+        }
+        console.log(GM_getValue('Options'));
+    }
+
+
     const transButton=document.createElement("div");
-    transButton.style="position: fixed;top: 1%; right:1%; width:70px; height:25px; border: thin;z-index:99999";
+    transButton.style="display: inline-flexbox; position: fixed;top: 1%; right:1%; margin: 0px; padding: 0px; border: thin; z-index:99999";
 
     const button1=document.createElement("button");
     button1.innerText="Tran";
-    button1.style= "width:50%; height:90%; top: 0; left: 0; position:absolute; border: none; text-align:right; padding:0px; margin: 0px;";
+    button1.style= "height:90%; border: none; text-align:right; padding: 5px 0px 5px 5px; margin:0px;";
     button1.onclick=mouseClick1;
     transButton.appendChild(button1);
 
     const button2=document.createElement("button");
     button2.innerText="slate";
-    button2.style= "width:50%; height:90%; top: 0; right: 0; position:absolute; border: none; text-align:left; padding:0px; margin: 0px;";
+    button2.style= "height:90%; border: none; text-align:left; padding: 5px 5px 5px 0px; margin:0px;";
     button2.onclick=mouseClick2;
     transButton.appendChild(button2);
 
+    const updateOptions = function (e) {
+        console.log(e);
+        switch (e.target.id) {
+            case 'chkNgoac':
+                Options.Ngoac=e.target.checked; break;
+            case 'chkMotnghia':
+                Options.VPmotnghia=e.target.checked; break;
+            case 'chkDichlieutru':
+                Options.Xoadichlieutru=e.target.checked; break;
+        } //end switch
+
+        GM_setValue("Options",JSON.stringify(Options));
+    }//End function
+
+    const menuBtn=document.createElement("button");
+    menuBtn.id='menuBtn';
+    menuBtn.innerText="↓"; // ↓  ↔
+    menuBtn.style= "height:90%; border: none; text-align:right; padding:5px 0px 5px 0px; margin:0px;";
+    menuBtn.onclick=menuClick;
+    transButton.appendChild(menuBtn);
+
     document.body.appendChild(transButton);
 
+    const menuOption=document.createElement('div');
+    menuOption.id='menuOption';
+    let menuOption_top= transButton.offsetTop + transButton.offsetHeight; //chua add vao document nen chua co gia tri
+    let menuOption_right= 5;
+    menuOption.style='display:none; top: '+ menuOption_top +'px; right: ' + menuOption_right +'px; position: fixed; background-color: lightblue; margin:4px 7px 7px 7px; padding:4px 7px 7px 7px; border-radius: 10px;z-index:999999;';
+
+    menuOption.innerHTML= '<fieldset style="border: 2px solid gray; padding: 5px; border-radius: 10px;"><legend>Các tùy chọn</legend>' +
+`<input type="checkbox" id="chkNgoac" ><label for="chkNgoac"> Dùng [Vietphrase] </label></br>` +
+`<input type="checkbox" id="chkMotnghia" ><label for="chkMotnghia"> Vietphrase một nghĩa </label></br>` +
+`<input type="checkbox" id="chkDichlieutru" ><label for="chkDichlieutru"> Xóa đích, liễu, trứ </label></br>` +
+'</fieldset>';
+
+    document.body.appendChild(menuOption);
+    document.getElementById('chkNgoac').onchange=updateOptions;
+    document.getElementById('chkMotnghia').onchange=updateOptions;
+    document.getElementById('chkDichlieutru').onchange=updateOptions;
+
+    //GM_deleteValue('Options');
+    let tmpOptions = GM_getValue('Options');
+    if (tmpOptions != null) Options=JSON.parse(tmpOptions);
+
+    document.getElementById('chkNgoac').checked=Options.Ngoac;
+    document.getElementById('chkMotnghia').checked=Options.VPmotnghia;
+    document.getElementById('chkDichlieutru').checked=Options.Xoadichlieutru;
 
 })();
