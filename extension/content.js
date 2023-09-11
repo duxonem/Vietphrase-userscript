f7fd5d34e873ed3b843d8cc2a092e8a76c33986d
duxonem <128269506+duxonem@users.noreply.github.com>
2023-09-11 09:17:58 +0700

    Add files via upload

--- extension/content.js
+++ extension/content.js
@@ -15,52 +15,57 @@ async function translateNode(rootNode) {
     }
 
     nodeToArr(rootNode);
-    chrome.runtime.sendMessage({ 'action': 'translate', 'payload': nodesText }, (mess) => {
-        let textArr = mess.payload.split(limiter);
-        textArr.forEach((text, index) => nodeArr[index].textContent = text);
-    });
+    if (Options.optionThayfont) {
+        const font = Options.optionFont.split(/[;,]/).filter(e => e != '').join() + ', Arial !important;';
+        const style = document.createElement('style');
+        style.appendChild(document.createTextNode(`body {font-family: ${font}}`));
+        document.head.appendChild(style);
+    }
+
+    let mess = await browser.runtime.sendMessage({ 'action': 'translate', 'payload': nodesText });
+    let textArr = mess.payload.split(limiter);
+    textArr.forEach((text, index) => nodeArr[index].textContent = text);
 }
 
-function loadOptions(fn) {
-    return chrome.runtime.sendMessage({ 'action': 'loadOptions', 'payload': '' }, fn)
+function loadOptions() {
+    return browser.runtime.sendMessage({ 'action': 'loadOptions', 'payload': '' })
 }
 
 let lang, percent;
-let charset = document.querySelector('meta[charset]')?.getAttribute('charset').toLowerCase();
-if (charset == undefined || !charset.includes('gb')) {
-    let metaContent = document.querySelector('meta[http-equiv="Content-Type"]')?.getAttribute('content')?.toLowerCase();
-    if (metaContent?.includes('charset')) charset = metaContent.slice(metaContent.indexOf('charset=') + 8); else charset = '';
-}
-if (charset.includes('gb')) lang = 'zh';
-else chrome.i18n.detectLanguage(document.title, (langInfo) => {
-    langInfo.languages.forEach(l => {
-        lang = l.language;
-        percent = l.percentage;
-        return;  //Escape from forEach
-    })
-});
-
-loadOptions((mess) => {
-    Options = mess.payload;
-    let webArr = [];
-    webArr = Options.optionWebsites.split(/[;,\n]/).map(w => w.trim());
-    let skipWeb = webArr.reduce((skip, web) => {
-        let reg = new RegExp(web, 'i');
-        return skip || reg.test(window.location.hostname);
-    }, false);
-
-    if (lang.includes('zh') && Options.optionAutoTrans && !skipWeb) translateNode(document);
-    else {
-        let btnTranslate = document.createElement('button');
-        btnTranslate.style.position = 'absolute';
-        btnTranslate.style.top = '2px';
-        btnTranslate.style.right = '2px';
-        btnTranslate.style.zIndex = '9999999';
-        btnTranslate.innerText = 'Translate'
-        document.body.appendChild(btnTranslate); //Tai sao khong them vao???
-        btnTranslate.onclick = () => { translateNode(document); btnTranslate.style.display = 'none' }
+browser.i18n.detectLanguage(document.title).then(langInfo => {
+    let charset = document.querySelector('meta[charset]')?.getAttribute('charset').toLowerCase();
+    if (charset == undefined || !charset.includes('gb')) {
+        let metaContent = document.querySelector('meta[http-equiv="Content-Type"]')?.getAttribute('content')?.toLowerCase();
+        if (metaContent?.includes('charset')) charset = metaContent.slice(metaContent.indexOf('charset=') + 8); else charset = '';
     }
+    if (charset.includes('gb')) lang = 'zh'; else
+        langInfo.languages.forEach(l => {
+            lang = l.language;
+            percent = l.percentage;
+            return;  //Escape from forEach
+        })
 
-    if (Options.optionThayfont)
-        document.body.style.fontFamily = Options.optionFont.split(/[;,]/).filter(e => e != '').join() + ', arial;';
+    loadOptions().then(mess => {
+        Options = mess.payload;
+        let webArr = [];
+        webArr = Options.optionWebsites.split(/[;,\n]/).map(w => w.trim());
+        let skipWeb = webArr.reduce((skip, web) => {
+            let reg = new RegExp(web, 'i');
+            return skip || reg.test(window.location.hostname);
+        }, false);
+
+        console.log(`lang: ${lang}`);
+        if (lang.includes('zh') && Options.optionAutoTrans && !skipWeb) translateNode(document);
+        else {
+            let btnTranslate = document.createElement('button');
+            btnTranslate.style.position = 'absolute';
+            btnTranslate.style.top = '2px';
+            btnTranslate.style.right = '2px';
+            btnTranslate.style.zIndex = '999999';
+            btnTranslate.innerText = 'Translate'
+            document.body.appendChild(btnTranslate);
+            btnTranslate.onclick = () => { translateNode(document);}
+        }
+    });
 });
+
